a29006c91d7ce63cb5d0a8e6d8d7bc737403fda6
MatteoMazzonelli <m.mazzonelli@gmail.com>
2022-05-19 12:09:46 +0000

    Useless flow entry removed

--- src/flow_entries_queues.sh
+++ src/flow_entries_queues.sh
@@ -8,7 +8,6 @@ sudo ovs-ofctl add-flow s1 ip,priority=2,nw_src=10.0.0.1,nw_dst=10.0.0.3,idle_ti
 # Flow entries switch 2
 sudo ovs-ofctl add-flow s2 ip,priority=2,nw_src=10.0.0.2,nw_dst=10.0.0.1,idle_timeout=0,actions=set_queue:123,normal
 sudo ovs-ofctl add-flow s2 ip,priority=2,nw_src=10.0.0.2,nw_dst=10.0.0.3,idle_timeout=0,actions=set_queue:345,normal
-sudo ovs-ofctl add-flow s2 ip,priority=2,nw_src=10.0.0.3,nw_dst=10.0.0.2,idle_timeout=0,actions=set_queue:123,normal
 
 # Flow entries switch 3
 sudo ovs-ofctl add-flow s3 ip,priority=2,nw_src=10.0.0.3,nw_dst=10.0.0.1,idle_timeout=0,actions=set_queue:123,normal
