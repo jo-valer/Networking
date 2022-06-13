aef6cff893a0dd1fae40d237b0af3e6eb6ce1c29
MatteoMazzonelli <m.mazzonelli@gmail.com>
2022-06-13 15:43:34 +0000

    Added UDP packet counter

--- scenario_2/run_controllers.sh
+++ scenario_2/run_controllers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ryu-manager slice1.py --observe-links --ofp-tcp-listen-port 6633 &
-ryu-manager slice2.py --observe-links --ofp-tcp-listen-port 6634 &
-ryu-manager slice3.py --observe-links --ofp-tcp-listen-port 6635 &
+ryu-manager slice_1.py --observe-links --ofp-tcp-listen-port 6633 &
+ryu-manager slice_2.py --observe-links --ofp-tcp-listen-port 6634 &
+ryu-manager slice_3.py --observe-links --ofp-tcp-listen-port 6635 &
 ryu-manager connect_slice.py --observe-links --ofp-tcp-listen-port 6636 &
