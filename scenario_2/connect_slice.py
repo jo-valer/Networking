5d09a037724484a3dadb3a102916cb5aa46bd773
dellastone <daniele.dellapietra@studenti.unitn.it>
2022-06-13 15:55:50 +0000

    set UDP count

--- scenario_2/connect_slice.py
+++ scenario_2/connect_slice.py
@@ -12,6 +12,10 @@ from ryu.lib.packet import ether_types
 from ryu.lib.packet import udp
 from ryu.lib.packet import tcp
 from ryu.lib.packet import icmp
+from ryu.lib.packet import ether_types
+from ryu.lib.packet import in_proto
+from ryu.lib.packet import ipv4
+
 import threading
 import time
 import copy
@@ -29,6 +33,7 @@ class SimpleSwitch(app_manager.RyuApp):
         self.host_slice1 = ["00:00:00:00:00:01","00:00:00:00:00:02","00:00:00:00:00:03","00:00:00:00:00:04"] 
         self.host_slice2 = ["00:00:00:00:00:05","00:00:00:00:00:06","00:00:00:00:00:07","00:00:00:00:00:08"]
         self.host_slice3 = ["00:00:00:00:00:09","00:00:00:00:00:0a","00:00:00:00:00:0b","00:00:00:00:00:0c"]
+        self.UDPsent =[0,0,0] # 1-2 , 1-3, 2-3  
         self.slice_to_port = {}
         self.mac_to_port = {}
         self.end_switches = []
@@ -74,44 +79,65 @@ class SimpleSwitch(app_manager.RyuApp):
         src = eth.src
         dpid = datapath.id
         # Switch 4 is off, packets are dropped 
-        print("Packet size -> ",msg.total_len)
-        
+       # print("Packet size -> ",msg.total_len)
+        if eth.ethertype == ether_types.ETH_TYPE_IP:
+            protocol = pkt.get_protocol(ipv4.ipv4)
+            protocol = protocol.proto
+            if protocol == in_proto.IPPROTO_UDP:
+                if (src in self.host_slice1 and dst in self.host_slice2) or (src in self.host_slice2 and dst in self.host_slice1):
+                    self.UDPsent[0] += 1
+                    if self.UDPsent[0] > 5000:
+                        print("Too many packet sent slice1-slice2")
+                        return
+                   # print("Total Byte sent from port ", msg.match["in_port"]," --> ",self.UDPsent[0])
+                if (src in self.host_slice2 and dst in self.host_slice3) or (src in self.host_slice3 and dst in self.host_slice2):
+                    self.UDPsent[1] += 1
+                    if self.UDPsent[1] > 5000:
+                        print("Too many packet sent slice2-slice3")
+                        return
+                   # print("Total Byte sent from port ", msg.match["in_port"]," --> ",self.UDPsent[1])
+                if (src in self.host_slice1 and dst in self.host_slice3) or (src in self.host_slice3 and dst in self.host_slice1):
+                    self.UDPsent[2] += 1
+                    if self.UDPsent[2] > 5000:
+                        print("Too many packet sent slice1-slice3")
+                        return
+                   # print("Total Byte sent from port ", msg.match["in_port"]," --> ",msg.total_len)
+                
+            self.mac_to_port.setdefault(dpid, {})
 
-        self.mac_to_port.setdefault(dpid, {})
+        # self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
 
-       # self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
+            # learn a mac address to avoid FLOOD next time.
+            
+            self.mac_to_port[dpid][src] = msg.match["in_port"]
 
-        # learn a mac address to avoid FLOOD next time.
-        
-        self.mac_to_port[dpid][src] = msg.match["in_port"]
-
-        if dpid in self.end_switches:
-            out_port = self.slice_to_port[dpid][msg.match["in_port"]]
-        elif dpid in self.mac_to_port:
-            if dst in self.mac_to_port[dpid]:
-                out_port = self.mac_to_port[dpid][dst]
+            if dpid in self.end_switches:
+                out_port = self.slice_to_port[dpid][msg.match["in_port"]]
+            elif dpid in self.mac_to_port:
+                if dst in self.mac_to_port[dpid]:
+                    out_port = self.mac_to_port[dpid][dst]
+                else:
+                    out_port = ofproto.OFPP_FLOOD
             else:
                 out_port = ofproto.OFPP_FLOOD
-        else:
-            out_port = ofproto.OFPP_FLOOD
 
-        # install a flow to avoid packet_in next time
-        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
-        if out_port != ofproto.OFPP_FLOOD and out_port != 0 and dst in self.hosts and src in self.hosts:
-            match = datapath.ofproto_parser.OFPMatch(in_port=msg.match["in_port"],eth_src=src,eth_dst=dst)
-           # self.add_flow(datapath, 1, match, actions)
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
+            # install a flow to avoid packet_in next time
+            actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+            if out_port != ofproto.OFPP_FLOOD and out_port != 0 and dst in self.hosts and src in self.hosts and protocol != in_proto.IPPROTO_UDP:
+                match = datapath.ofproto_parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,ip_proto=protocol,in_port=msg.match["in_port"],eth_src=src,eth_dst=dst)
+                self.add_flow(datapath, 1, match, actions)
+            data = None
+            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                data = msg.data
 
-        out = datapath.ofproto_parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
-            actions=actions, data=data)
-        datapath.send_msg(out)
-
-        if out_port!=0:
-           # self.logger.info("LOG s%s sending packet (out_port=%s)", dpid, out_port)
+            out = datapath.ofproto_parser.OFPPacketOut(
+                datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
+                actions=actions, data=data)
             datapath.send_msg(out)
+
+            if out_port!=0:
+            # self.logger.info("LOG s%s sending packet (out_port=%s)", dpid, out_port)
+                datapath.send_msg(out)
 
 
     @set_ev_cls(event.EventSwitchEnter)
